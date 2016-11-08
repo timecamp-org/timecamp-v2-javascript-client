c10b00818e19ea0820cdeecc008fd3926408f187
Maciej Jurga <m.jurga@timecamp.com>
2016-11-08 14:54:55 +0100

    added babel-regenerator-runtime

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -3,20 +3,20 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = require('./webpack.base.babel')({
-  // In production, we skip all hot-reloading stuff
-  entry: [
-    path.join(process.cwd(), 'lib/index.js'),
-  ],
+    // In production, we skip all hot-reloading stuff
+    entry: [
+        'babel-regenerator-runtime',
 
-  // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
-  output: {
-    path: "dist",
-    filename: "timecamp-api.[name].js",
-    library: ["timecamp-api", "[name]"],
-    libraryTarget: "umd"
-  },
-  devtool: 'source-map',
-  plugins: [
+        path.join(process.cwd(), 'lib/index.js'),
+    ],
 
-  ],
+    // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
+    output: {
+        path: "dist",
+        filename: "timecamp-api.[name].js",
+        library: ["timecamp-api", "[name]"],
+        libraryTarget: "umd"
+    },
+    devtool: 'source-map',
+    plugins: [],
 });
